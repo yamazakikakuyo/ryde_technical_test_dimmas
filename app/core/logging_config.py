430d961805ab6a26d3e5184f8b09bc627b176e10
Dimmas Mulya <dimmas3010@gmail.com>
2025-06-25 11:07:13 +0700

    Fix Loggin Configuration

--- app/core/logging_config.py
+++ app/core/logging_config.py
@@ -1,11 +1,18 @@
 import logging
+import os
+
+LOG_DIR = "logs"
+LOG_FILE = os.path.join(LOG_DIR, "app.log")
+
+# Ensure the logs directory exists
+os.makedirs(LOG_DIR, exist_ok=True)
 
 # Basic logger config
 logging.basicConfig(
     level=logging.INFO,  # Change to DEBUG for more verbosity
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("logs/app.logs"),
+        logging.FileHandler("logs/app.log"),
         logging.StreamHandler()
     ]
 )
